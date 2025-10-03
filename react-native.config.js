d0b47975d4b89b356e9f3a2e15fc0f1635bacd73
Tofunmi Onaeko <tofunmionaeko@192.168.1.155>
2025-10-03 17:57:55 +0100

    fixed broken maps on iOS

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   dependencies: {
+//     'react-native-maps': {
+//       platforms: {
+//         ios: {
+//           podspecPath: '../node_modules/react-native-maps/react-native-google-maps.podspec',
+//           sharedLibraries: ['GoogleMaps', 'Google-Maps-iOS-Utils'],
+//         },
+//       },
+//     },
+//   },
+// };
