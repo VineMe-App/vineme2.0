3d33343adf29657c423af3abc3494f581f5c03e7
Tom Oakley <toakley@totallymoney.com>
2025-11-26 20:36:51 +0000

    re-add shouldShowAlert

--- src/services/notifications.ts
+++ src/services/notifications.ts
@@ -14,6 +14,7 @@ import type {
 // Configure notification behavior
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
+    shouldShowAlert: true,
     shouldPlaySound: true,
     shouldSetBadge: true,
     shouldShowBanner: true,
